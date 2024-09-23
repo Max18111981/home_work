619c1c0061b93929a15fa578e3230810ed1d6cab
Max18111981 <maksplastinin81@gmail.com>
2024-09-23 12:37:52 +0700

    Домашнее задание

---
+++ pythonProject11/main.py
@@ -0,0 +1,193 @@
+# Реализуйте класс «Автомобиль». Необходимо хранить
+# в полях класса: название модели, год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте
+# методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса
+
+# class Car:
+#     def __init__(self, model, year, manufacturer, engine_volume, color, price):
+#         self.__model = model
+#         self.__year = year
+#         self.__manufacturer = manufacturer
+#         self.__engine_volume = engine_volume
+#         self.__color = color
+#         self.__price = price
+#
+#     def input_data(self):
+#         self.__model = input('Введите название модели: ')
+#         self.__year = input('Введите год выпуска: ')
+#         self.__manufacturer = input('Введите производителя: ')
+#         self.__engine_volume = input('Введите объем двигателя: ')
+#         self.__color = input('Введите цвет машины: ')
+#         self.__price = input('Введите цену: ')
+#
+#     def output_data(self):
+#         print(f'Модель: {self.model}')
+#         print(f'Год выпуска: {self.year}')
+#         print(f'Производитель: {self.manufacturer}')
+#         print(f'Объем двигателя: {self.engine_volume}')
+#         print(f'Цвет машины: {self.color}')
+#         print(f'Цена: {self.price}')
+#
+#     def get_model(self):
+#         return self.__model
+#
+#     def get_year(self):
+#         return self.__year
+#
+#     def get_manufacturer(self):
+#         return self.__manufacturer
+#
+#     def get_engine_volume(self):
+#         return self.__engine_volume
+#
+#     def get_color(self):
+#         return self.__color
+#
+#     def get_price(self):
+#         return self.__price
+#
+#     def set_model(self, model):
+#         self.__model = model
+#
+#     def set_year(self, year):
+#         self.__year = year
+
+    # def set_manufacturer(self, manufacturer):
+    #     self.__manufacturer = manufacturer
+    #
+    # def set_engine_volume(self, engine_volume):
+    #     self.__engine_volume = engine_volume
+    #
+    # def set_color(self, color):
+    #     self.__color = color
+    #
+    # def set_price(self, price):
+    #     self.__price = price
+
+
+
+# Реализуйте класс «Книга». Необходимо хранить в
+# полях класса: название книги, год выпуска, издателя,
+# жанр, автора, цену. Реализуйте методы класса для ввода
+# данных, вывода данных, реализуйте доступ к отдельным
+# полям через методы класса.
+
+# class Book:
+#     def __init__(self, title, year, publisher, genre, author, price):
+#         self.__title = title
+#         self.__year = year
+#         self.__publisher = publisher
+#         self.__genre = genre
+#         self.__author = author
+#         self.__price = price
+#
+#     def input_data(self):
+#         self.__title = input('Введите название книги: ')
+#         self.__year = input('Введите год выпуска: ')
+#         self.__publisher = input('Введите издателя: ')
+#         self.__genre = input('Введите жанр: ')
+#         self.__author = input('Введите автора: ')
+#         self.__price = input('Введите цену: ')
+#
+#     def output_data(self):
+#         print(f'Название книги: {self.title}')
+#         print(f'Год выпуска: {self.year}')
+#         print(f'Издатель: {self.publisher}')
+#         print(f'Жанр: {self.genre}')
+#         print(f'Автор: {self.author}')
+#         print(f'Цена: {self.price}')
+#
+#     def get_title(self):
+#         return self.__title
+#
+#     def get_year(self):
+#         return self.__year
+#
+#     def get_publisher(self):
+#         return self.__publisher
+#
+#     def get_genre(self):
+#         return self.__genre
+#
+#     def get_author(self):
+#         return self.__author
+#
+#     def get_price(self):
+#         return self.__price
+#
+#     def set_title(self, title):
+#         self.__title = title
+#
+#     def set_year(self, year):
+#         self.__year = year
+#
+#     def set_publisher(self, publisher):
+#         self.__publisher = publisher
+#
+#     def set_genre(self, genre):
+#         self.__genre = genre
+#
+#     def set_author(self, author):
+#         self.__author = author
+#
+#     def set_price(self, price):
+#         self.__price = price
+
+
+# Реализуйте класс «Стадион». Необходимо хранить в
+# полях класса: название стадиона, дату открытия, страну,
+# город, вместимость. Реализуйте методы класса для ввода
+# данных, вывода данных, реализуйте доступ к отдельным
+# полям через методы класса.
+
+# class Stadium:
+#     def __init__(self, name, opening_date, country, city, capacity):
+#         self.__name = name
+#         self.__opening_date = opening_date
+#         self.__country = country
+#         self.__city = city
+#         self.__capacity = capacity
+#
+#     def input_data(self):
+#         self.__name = input('Введите название стадиона: ')
+#         self.__opening_date = input('Введите дату открытия: ')
+#         self.__country = input('Введите страну: ')
+#         self.__city = input('Введите город: ')
+#         self.__capacity = input('Введите вместимость: ')
+#
+#     def output_data(self):
+#         print(f'Название стадиона: {self.name}')
+#         print(f'Дата открытия: {self.opening_date}')
+#         print(f'Страна: {self.country}')
+#         print(f'Город: {self.city}')
+#         print(f'Вместимость: {self.capacity}')
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def get_opening_date(self):
+#         return self.__opening_date
+#
+#     def get_country(self):
+#         return self.__country
+#
+#     def get_city(self):
+#         return self.__city
+#
+#     def get_capacity(self):
+#         return self.__capacity
+#
+#     def set_name(self, name):
+#         self.__name = name
+#
+#     def set_opening_date(self, opening_date):
+#         self.__opening_date = opening_date
+#
+#     def set_country(self, country):
+#         self.__country = country
+#
+#     def set_city(self, city):
+#         self.__city = city
+#
+#     def set_capacity(self, capacity):
+#         self.__capacity = capacity
+
