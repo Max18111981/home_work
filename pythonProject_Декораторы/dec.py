8dc44b647844ad9fdb009a6dc8a43885f8a6225d
Max18111981 <maksplastinin81@gmail.com>
2024-10-08 12:38:11 +0700

    Create dec.py

---
+++ pythonProject_Декораторы/dec.py
@@ -0,0 +1,39 @@
+# Создайте функцию, возвращающую список со всеми
+# простыми числами от 0 до 1000.
+# Используя механизм декораторов посчитайте сколько
+# секунд, потребовалось для вычисления всех простых чисел.
+# Отобразите на экран количество секунд и простые числа.
+
+
+import time
+from functools import wraps
+
+
+def timer_decorator(func):
+    @wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.time()
+        value = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        print(f"Finished in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+@timer_decorator
+def get_primes(n):
+    primes = []
+    for possiblePrime in range(2, n + 1):
+        isPrime = True
+        for num in range(2, int(possiblePrime ** 0.5) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(possiblePrime)
+    return primes
+
+
+primes = get_primes(1000)
+print("Prime numbers:", primes)
